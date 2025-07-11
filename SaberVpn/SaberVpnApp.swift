7777bf684e06f92e65c8f5baf194c8b6ca179a0a
Vladimir <vladimirzakharovj@yandex.ru>
2025-07-11 06:26:46 -0700

    Initial Commit

---
+++ SaberVpn/SaberVpnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaberVpnApp.swift
+//  SaberVpn
+//
+//  Created by Vladimir on 11.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct SaberVpnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
